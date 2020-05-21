a5bb5bc1f8e75a561bf622c0c3cd4870fb7d92f2
KLEEEEEER <gonzalx_747@mail.ru>
2020-05-21 19:06:45 +0900

    Basic tutorials on photon completed

--- Assets/Tutorial/Scripts/NetworkCallbacks.cs
+++ Assets/Tutorial/Scripts/NetworkCallbacks.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Bolt;
 
-public class NetworkCallbacks : MonoBehaviour
+[BoltGlobalBehaviour]
+public class NetworkCallbacks : GlobalEventListener
 {
-    // Start is called before the first frame update
-    void Start()
+    List<string> logMessages = new List<string>();
+
+    public override void SceneLoadLocalDone(string scene)
     {
-        
+        var spawnPosition = new Vector3(Random.Range(-8, 8), 0, Random.Range(-8, 8));
+
+        BoltNetwork.Instantiate(BoltPrefabs.Robot, spawnPosition, Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEvent(LogEvent evnt)
     {
-        
+        logMessages.Insert(0, evnt.Message);
+    }
+
+    void OnGUI()
+    {
+        // only display max the 5 latest log messages
+        int maxMessages = Mathf.Min(5, logMessages.Count);
+
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 200, Screen.height - 100, 400, 100), GUI.skin.box);
+
+        for (int i = 0; i < maxMessages; ++i)
+        {
+            GUILayout.Label(logMessages[i]);
+        }
+
+        GUILayout.EndArea();
     }
 }
