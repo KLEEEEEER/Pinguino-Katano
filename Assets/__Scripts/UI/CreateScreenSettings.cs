6dc288f455f9ed65c7090c5c64f193b578e771f7
KLEEEEEER <gonzalx_747@mail.ru>
2021-01-26 14:49:22 +0900

    Old. Started creating settings

---
+++ Assets/__Scripts/UI/CreateScreenSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateScreenSettings : MonoBehaviour
+{
+    public void OnRoundsChanged(float value)
+    {
+        PlayerPrefs.SetInt("Rounds", (int)value);
+    }
+}
---
+++ Assets/__Scripts/UI/SliderBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class SliderBehaviour : MonoBehaviour
+{
+    Text textComponent;
+    private void Start()
+    {
+        textComponent = GetComponent<Text>();
+    }
+
+    public void OnSliderChanged(float value)
+    {
+        textComponent.text = value.ToString();
+    }
+}
